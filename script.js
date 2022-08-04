b7bf12f25092e47d54ede90a5b67a69033f9955e
swardhaSawant <swardhasawant890@gmail.com>
2022-08-04 11:36:52 +0530

    refactored code

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
+// initializing the elements
 const button = document.querySelector("#search");
 const paragraph = document.querySelector("#para");
+// url for random jokes
 const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
-
+// feteching the data from url
 const randomJoke = ()=>{
     fetch(url)
     .then(res => res.json())
     .then((data)=>{
-        // console.log(data.joke);
         paragraph.innerText = data.joke;
     })
 }
-
+// added event to button
 button.addEventListener('click', randomJoke)
 
