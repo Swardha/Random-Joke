045411e6a082f5c834b212a5b6c75000a2da2f23
swardhaSawant <swardhasawant890@gmail.com>
2022-08-04 16:22:27 +0530

    added style

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ const paragraph = document.querySelector("#para");
 const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
 // feteching the data from url
 const randomJoke = ()=>{
+    paragraph.classList.remove("fade");
     fetch(url)
     .then(res => res.json())
     .then((data)=>{
         paragraph.innerText = data.joke;
+        paragraph.classList.add("fade");
     })
 }
 // added event to button
